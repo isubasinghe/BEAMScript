814c66a2ba7c24a64d46bf93fb07dbfd4198030b
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2020-10-20 14:27:06 +1100

    feat: added function partial compilation

--- src/BSCodeGen/LLVM.hs
+++ src/BSCodeGen/LLVM.hs
@@ -3,30 +3,55 @@ module BSCodeGen.LLVM where
 import qualified BSAST as BSAST
 import Data.String
 import LLVM.AST
-import LLVM.Pretty (ppllvm)
+import qualified LLVM.AST.CallingConvention as CC
+import qualified LLVM.AST.Global as G
+import qualified LLVM.AST.Linkage as L
+import qualified LLVM.AST.Visibility as V
 
 getType :: BSAST.VarType -> Type
 getType (BSAST.VBool) = IntegerType {typeBits = 8} -- one byte for booleans
 getType (BSAST.VInt) = IntegerType {typeBits = 64} -- only support 64 bit
 getType (BSAST.VType s) = NamedTypeReference (mkName s)
-
-getTypes :: [BSAST.VarType] -> [Type]
-getTypes [] = []
-getTypes (t : ts) = (getType t) : (getTypes ts)
+getType (BSAST.Void) = VoidType
 
 compileStruct :: BSAST.TypeDecl -> Definition
 compileStruct (BSAST.TypeDecl n t) = TypeDefinition (mkName n) (Just StructureType {isPacked = False, elementTypes = types})
   where
-    types = getTypes (map snd t)
+    types = (map (getType . snd) t)
+
+compileFunction :: BSAST.Function -> Definition
+compileFunction (BSAST.Function n ps r ss) =
+  GlobalDefinition
+    ( Function
+        { G.linkage = L.External,
+          G.visibility = V.Default,
+          G.dllStorageClass = Nothing,
+          G.callingConvention = CC.C,
+          G.returnAttributes = [],
+          G.returnType = getType r,
+          G.name = mkName n,
+          G.parameters = ([], False),
+          G.functionAttributes = [],
+          G.section = Nothing,
+          G.comdat = Nothing,
+          G.alignment = 0,
+          G.garbageCollectorName = Nothing,
+          G.prefix = Nothing,
+          G.basicBlocks = [],
+          G.personalityFunction = Nothing,
+          G.metadata = []
+        }
+    )
 
 compile :: BSAST.Program -> Module
-compile (BSAST.Program m f ss fs) =
+compile (BSAST.Program f m ss fs) =
   Module
     { moduleName = fromString m,
       moduleSourceFileName = fromString f,
       moduleDataLayout = Nothing,
       moduleTargetTriple = Nothing,
-      moduleDefinitions = structs
+      moduleDefinitions = structs ++ functinos
     }
   where
     structs = map compileStruct ss
+    functinos = map compileFunction fs
