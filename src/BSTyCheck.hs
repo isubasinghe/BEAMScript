3248da2f9678e1e77ee31084e0f79add9f8613e1
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2023-03-26 22:23:52 +1100

    more type checking code

--- src/BSTyCheck.hs
+++ src/BSTyCheck.hs
@@ -2,9 +2,10 @@ module BSTyCheck where
 import BSAST
 import Control.Monad.State
 import Control.Monad.Except ( ExceptT, MonadError(throwError) )
+import Data.Map (Map)
 
 
-data TypeCtx = TypeCtx {}
+data TypeCtx = TypeCtx { fns :: Map String ([VarType], VarType) }
 
 data TyError = TyError String
 
@@ -12,23 +13,51 @@ type TyCtx = ExceptT TyError (State TypeCtx)
 
 
 liftAssert :: (Eq a, Show a) => a -> a -> TyCtx ()
-liftAssert a b = if a == b then pure () else throwError $ TyError (show a ++ "!=" ++ show b)
+liftAssert a b = if a == b then pure () else throwError $ TyError (show a ++ " != " ++ show b)
 
-checkStatement :: Statement -> TyCtx ()
+
+liftAssertDistinct :: [Maybe VarType] -> TyCtx (Maybe VarType)
+liftAssertDistinct [] = pure Nothing
+liftAssertDistinct [a] = pure a
+liftAssertDistinct (a:b:as) = do 
+  if a == b then 
+    liftAssertDistinct (b:as)
+  else 
+    throwError $ TyError (show a ++ " != " ++ show b) 
+
+
+checkStatement :: Statement -> TyCtx (Maybe VarType)
 checkStatement (If cond rss lss as) = do
     condty <- checkExpr cond
     liftAssert condty VBool
     es <- mapM (\(e, _) -> checkExpr e) lss
     mapM_ (`liftAssert` VBool) es
+    rstys <- mapM checkStatement rss
+    rstys' <- liftAssertDistinct rstys
+    lsstys <-  mapM (\(e, ss) -> mapM checkStatement ss) lss
+    lsstys' <- mapM liftAssertDistinct lsstys
+    lsstys'' <- liftAssertDistinct lsstys'
+    astys <- mapM checkStatement as
+    astys' <- liftAssertDistinct astys
+    liftAssertDistinct [rstys', lsstys'', astys']
 checkStatement (For cond ss) = do 
     condty <- checkExpr cond
     liftAssert condty VBool 
+    pure Nothing 
 
-checkStatement (Decl ident varty) = undefined
-checkStatement (AssignRecord r vs) = undefined
-checkStatement (AssignDecl id ty e) = undefined
-checkStatement (Assign lhs rhs) = undefined
-checkStatement (Call ident args) = undefined
+checkStatement (Decl ident varty) = pure Nothing
+checkStatement (AssignRecord r vs) = pure Nothing
+checkStatement (AssignDecl id ty e) = pure Nothing
+checkStatement (Assign lhs rhs) = do 
+  lhsty <- checkExpr lhs 
+  rhsty <- checkExpr rhs 
+  liftAssert lhsty rhsty
+  pure Nothing
+
+checkStatement (Call ident args) = do
+  argsty <- mapM checkExpr args 
+  x <- get
+  undefined
 checkStatement (ReturnExpr e) = undefined
 checkStatement Return = undefined
 checkStatement (Block ss) = undefined
